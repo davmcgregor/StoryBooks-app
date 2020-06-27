59245fecaaa98bbac00ffdb752a8d1facc3c0a9a
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-06-27 22:55:35 +0100

    Set up config

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const dotenv = require("dotenv");
 // Load config
 dotenv.config({ path: "./config.config.env" });
 
-const app = express()
+const app = express();
 
-app.listen()
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
