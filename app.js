e8ace7cbb73cc77c32b1206df2ad6ca557150c8d
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-06-27 23:17:00 +0100

    Update config2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 
 // Load config
-dotenv.config({ path: "./config.config.env" });
+dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
