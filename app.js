38b7fe73fcdb392d035701dee526728a7b806c66
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-06-29 19:08:22 +0100

    Add editIcon helper

--- app.js
+++ app.js
@@ -29,13 +29,18 @@ if (process.env.NODE_ENV === "development") {
 }
 
 // Handlebars Helpers
-const { formatDate, stripTags, truncate } = require("./helpers/hbs");
+const { formatDate, stripTags, truncate, editIcon } = require("./helpers/hbs");
 
 // Handlebars
 app.engine(
   ".hbs",
   exphbs({
-    helpers: { formatDate, stripTags, truncate },
+    helpers: {
+      formatDate,
+      stripTags,
+      truncate,
+      editIcon,
+    },
     defaultLayout: "main",
     extname: ".hbs",
   })
@@ -55,6 +60,12 @@ app.use(
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Set global var
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
 
 // Static folder
 app.use(express.static(path.join(__dirname, "public")));
